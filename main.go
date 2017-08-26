d172864569db651b07dad2af192f24dfab4f7e6f
shavenking <shavenking@gmail.com>
2017-08-26 19:47:24 +0800

    Add markdown support

--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
 )
 
-func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	fmt.Println("Starting Server...")
+func renderMarkdown(response http.ResponseWriter, request *http.Request) {
+	content, err := ioutil.ReadFile("./static/" + request.URL.Path + ".md")
+	if err != nil {
+		content, err = ioutil.ReadFile("./static/index.md")
+		if err != nil {
+			http.NotFound(response, request)
+		}
+	}
 
-	http.ListenAndServe(":80", nil)
+	unsafe := blackfriday.MarkdownCommon(content)
+
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response.Write(unsafe)
+}
+
+func main() {
+	h := http.NewServeMux()
+
+	h.HandleFunc("/", renderMarkdown)
+
+	fmt.Println("Starting Server...")
+	http.ListenAndServe(":80", h)
 }
