a54a40a8e90b9a4d75d02e81886740fe75f18b32
shavenking <shavenking@gmail.com>
2017-09-24 10:48:18 +0800

    Add github.go

---
+++ github.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type GitHub struct {}
+
+type Repo struct {
+	Id int `json:"id"`
+	FullName string `json:"full_name"`
+}
+
+func (g GitHub) GetRepos() ([]Repo, error) {
+	resp, err := http.Get("https://api.github.com/users/shavenking/repos")
+	if err != nil {
+		return []Repo{}, err
+	}
+
+	defer resp.Body.Close()
+
+	repos := make([]Repo, 0)
+	json.NewDecoder(resp.Body).Decode(&repos)
+
+	return repos, nil
+}
