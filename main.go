5358b2da5f28c1fe217797415b47151a26317871
shavenking <shavenking@gmail.com>
2017-09-24 11:54:15 +0800

    Yo index page

--- main.go
+++ main.go
@@ -6,7 +6,23 @@ import (
 	"io/ioutil"
 	"fmt"
 	"html/template"
+	"strings"
 )
+
+func renderIndex(response http.ResponseWriter, request *http.Request) {
+	t, _ := template.ParseFiles("./templates/index.html")
+
+	files, _ := ioutil.ReadDir("./articles")
+
+	articles := make([]string, len(files))
+
+	for i, file := range files {
+		articles[i] = strings.TrimSuffix(file.Name(), ".md")
+	}
+
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t.Execute(response, articles)
+}
 
 func renderRepos(response http.ResponseWriter, request *http.Request) {
 	g := GitHub{}
@@ -38,6 +54,7 @@ func renderArticle(response http.ResponseWriter, request *http.Request) {
 func main() {
 	h := http.NewServeMux()
 
+	h.HandleFunc("/", renderIndex)
 	h.HandleFunc("/repos/", renderRepos)
 	h.HandleFunc("/articles/", renderArticle)
 
